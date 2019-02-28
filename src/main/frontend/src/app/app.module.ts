65c14f197f291b74755869a071f04a22176aee7a
Menik <menikdisney@gmail.com>
2019-02-28 11:37:47 +0600

    Creating angular components

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskAddComponent } from './tasks/task-add/task-add.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskAddComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
